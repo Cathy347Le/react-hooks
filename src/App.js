383fac5e46efc1ba50a00901b9a33c5197c0b845
Cathy Le <cathyle@cathys-mbp.lan>
2021-12-14 23:16:20 -0500

    use axios to display api data on screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [randomUserData, setRandomUserData] = useState("");
 
   const IncrementCounter = () => {
     setCounter(counter + 1);
@@ -13,15 +15,26 @@ function App() {
   };
 
   const fetchRandomData = () => {
-    fetch("https://randomuser.me/api")
-      .then((res) => {
-        console.log(res.json());
-        return res;
+    return axios
+      .get("https://randomuser.me/api")
+      .then((data) => {
+        //handle success
+        console.log(data);
+        // return JSON.stringify(data);
+        return JSON.stringify(data, null, 2); //can take 3 parameters, value, replacer, and space
       })
       .catch((err) => {
+        //handle error
         console.log(err);
       });
   };
+
+  useEffect(() => {
+    fetchRandomData().then((randomData) => {
+      // setRandomUserData(randomData);
+      setRandomUserData(randomData || "No user data found.");
+    });
+  }, []);
 
   return (
     <div className="App">
@@ -34,12 +47,17 @@ function App() {
         <button className="m-2" onClick={DecrementCounter}>
           Decrement
         </button>
-      </div>
-      <div className="api-data-container mt-5">
-        <button onClick={fetchRandomData}>Get Random User</button>
+        <div className="api-data-container">
+          <pre>{randomUserData}</pre>
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
+
+/*<button className="mt-5" onClick={fetchRandomData}>
+Fetch Random user
+</button>
+*/
